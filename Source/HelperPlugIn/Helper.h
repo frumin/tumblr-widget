1f524e3754eb358f1d9cf7d8afde9849d5a1f9e0
zaworowski <zaworowski@dedb6aec-9e2c-0410-83a2-1d91b0e205d1>
2009-10-25 03:45:19 +0000

    Added HelperPlugIn Cocoa bundle to handle Growl notification and eventually all upload code

---
+++ Source/HelperPlugIn/Helper.h
@@ -0,0 +1,22 @@
+//
+//  Helper.h
+//  HelperPlugIn
+//
+//  Created by frumin on 10/22/09.
+//  Copyright 2009 LSD Programming. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Growl/Growl.h>
+#import <WebKit/WebKit.h>
+
+@interface Helper : NSObject <GrowlApplicationBridgeDelegate> {
+}
+
+- (void)growlUploadComplete;
+- (void)growlUploadFailure;
+
+- (void)web_growlUploadComplete;
+- (void)web_growlUploadFailure;
+
+@end
